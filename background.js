f774e607456c027e59177df45783c4469a7859e2
shubham agrawal <manovagyanik1@gmail.com>
2017-05-31 19:39:02 +0530

    working scheduling

--- background.js
+++ background.js
@@ -1,10 +1,31 @@
- var schedulePost = function(word){
-    var query = word.selectionText;
-    chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + query});
+
+
+ var schedulePost = function(image){
+     console.log(image.srcUrl);
+     var success = function(){
+        console.log("successfully posted");
+    }
+    var pageId = 1884816475065657;
+    var baseDomain = "https://graph.facebook.com";
+    var pageAccessToken = "EAAEw28ggKzcBAEQFUt6qvii3IDHEZA2G1WtMlDJEDEM11BqhbbwgOEz3hgAPwnGkZCZAuEquDVpdZB9f5Sk4xgAVeiv4RN9h2XFBNP5FniqvOjB3bId0Pt53eYbwM9aBm3GUQb5EJZCzfiEHAq3mGxZBP2OUJZCp380lPtO7mQdhgZDZD";
+    var url = baseDomain+"/"+pageId+"/photos?access_token="+pageAccessToken;
+    var data = {
+        "message": "test post 4",
+        "page_access_token": pageAccessToken,
+        "url": image.srcUrl, 
+    };
+    var urlSanitized = encodeURIComponent(url);
+    $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    success: success,
+    dataType: "json"
+    });
  };
 
 chrome.contextMenus.create({
  title: "Schedule post",
- contexts:["image", "video"],  // ContextType.. video context menu is somehow fucking up.. mostly because fb is overriding it
+ contexts:["all"],  // ContextType.. video context menu is somehow fucking up.. mostly because fb is overriding it
  onclick: schedulePost // A callback function
 });
